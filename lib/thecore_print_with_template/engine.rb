d9997095a6fe402dfa10f276fa72b42d6532136b
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2019-04-30 16:36:23 +0200

    migration initializer for engine cleanup

--- lib/thecore_print_with_template/engine.rb
+++ lib/thecore_print_with_template/engine.rb
@@ -2,7 +2,7 @@ module ThecorePrintWithTemplate
   class Engine < ::Rails::Engine
 
     initializer 'thecore_print_with_template.add_to_migrations' do |app|
-      unless app.root.to_s == root.to_s
+      unless app.root.to_s.match root.to_s
         # APPEND TO MAIN APP MIGRATIONS FROM THIS GEM
         config.paths['db/migrate'].expanded.each do |expanded_path|
           app.config.paths['db/migrate'] << expanded_path
