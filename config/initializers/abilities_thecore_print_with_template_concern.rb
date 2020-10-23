5bcfe5354be9869666fa5a4ab0943522526f5cb9
Gabriele Tassoni <gabrieletassoni@alchemic.it>
2020-10-23 14:58:21 +0000

    Add new ability version

--- config/initializers/abilities_thecore_print_with_template_concern.rb
+++ config/initializers/abilities_thecore_print_with_template_concern.rb
@@ -1,23 +1,20 @@
-
-require 'active_support/concern'
-
-module ThecorePrintWithTemplateAbilitiesConcern
-    extend ActiveSupport::Concern
-    included do
-    def thecore_print_with_template_abilities user
-        if user
-        # if the user is logged in, it can do certain tasks regardless his role
-        if user.admin?
-            # if the user is an admin, it can do a lot of things, usually
+module Abilities
+    class ThecorePrintWithTemplatesCommon
+        include CanCan::Ability
+        def initialize user
+            if user
+                # if the user is logged in, it can do certain tasks regardless his role
+                if user.admin?
+                    # if the user is an admin, it can do a lot of things, usually
+                end
+                
+                if user.has_role? :role
+                    # a specific role, brings specific powers
+                end
+            end
         end
-
-        if user.has_role? :role
-            # a specific role, brings specific powers
-        end
-        end
-    end
     end
 end
 
 # include the extension
-TheCoreAbilities.send(:include, ThecorePrintWithTemplateAbilitiesConcern)
+# TheCoreAbilities.send(:include, ThecorePrintWithTemplateAbilitiesConcern)
